305b2c5a1fa85eba64ae19a675c11e433cd827ef
Saki Jono <kinokinoartist12@gmail.com>
2021-03-18 23:27:55 +0900

    count3 data extraction

--- script.js
+++ script.js
@@ -7,28 +7,34 @@ clear.onclick = function () {
 }
 
 
+
 //peace表示
-function peace(landmarks) {
+function peace(landmarks, isRightHand) {
   const text = document.getElementById('text');
-  const endtext = text.value[text.value.length - 1]
-  if (landmarks[2].x < landmarks[4].x && landmarks[14].y < landmarks[16].y && landmarks[18].y < landmarks[20].y && landmarks[8].y < landmarks[7].y && landmarks[12].y < landmarks[11].y) {
-    if (endtext != 'e' && landmarks[9].x - landmarks[5].x < landmarks[12].x - landmarks[8].x) {
-      text.innerHTML = text.innerHTML + 'peace';
-    } else if (endtext != 'う' && landmarks[9].x - landmarks[5].x > landmarks[12].x - landmarks[8].x) {
-      text.innerHTML = text.innerHTML + 'う';
-      console.log("う")
-    }
-  } else if (landmarks[4].x < landmarks[2].x && landmarks[6].y < landmarks[8].y && landmarks[10].y < landmarks[12].y && landmarks[14].y < landmarks[16].y && landmarks[18].y < landmarks[20].y) {
-    if (endtext != 'あ') {
-      text.innerHTML = text.innerHTML + 'あ';
-    }
-  } else if (landmarks[3].x < landmarks[4].x && landmarks[6].y < landmarks[8].y && landmarks[10].y < landmarks[12].y && landmarks[14].y < landmarks[16].y && landmarks[20].y < landmarks[18].y) {
-    if (endtext != 'い') {
-      text.innerHTML = text.innerHTML + 'い';
-    }
-  } else if (landmarks[3].x < landmarks[4].x && landmarks[6].y < landmarks[8].y && landmarks[10].y < landmarks[12].y && landmarks[14].y < landmarks[16].y && landmarks[18].y < landmarks[20].y) {
-    if (endtext != 'え') {
-      text.innerHTML = text.innerHTML + 'え';
-    }
+  if (landmarks) {
+    text.textContent = "ok";
   };
+
+  //3秒間記録
+  const count3 = document.getElementById('count3');
+  const handsarray = document.getElementById('handsarray');
+  if (count3.classList.contains('record') == true) {
+    if (isRightHand == true) {
+      handsarray.innerHTML = handsarray.innerHTML += landmarks[0].x + "right" + "</br>";
+    } else {
+      handsarray.innerHTML = handsarray.innerHTML += landmarks[0].x + "left" + "</br>";
+    }
+    console.log(landmarks, isRightHand);
+  }
+
+  count3.onclick = function () {
+    count3.style.color = "red";
+    count3.classList.toggle('record');
+    function timeup() {
+      count3.style.color = "#000000";
+      count3.classList.toggle('record');
+    };
+    setTimeout(timeup, 3000);
+  }
+
 }
